f596155cbafa8fd07c309c6be8ea85074e3151be
patrick <patrick.oysteinsen@gmail.com>
2025-06-29 15:10:17 +0200

    Add GlowingTotemsClient class to initialize client-side functionality for the Glowing Totems mod

---
+++ src/client/java/glowingtotems/GlowingTotemsClient.java
@@ -0,0 +1,10 @@
+package glowingtotems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlowingTotemsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// No code needed; dynamic lighting is handled via JSON data pack.
+	}
+}
